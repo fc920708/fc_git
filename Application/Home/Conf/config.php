1a30cbc6bb495cf74b88fdd2689afed8b009df4a
fengchao <280315697@qq.com>
2017-02-14 11:15:35 +0800

    404

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    /* 错误页面模板 */
+    'TMPL_ACTION_ERROR'     =>  MODULE_PATH.'View/Public/error.html', // 默认错误跳转对应的模板文件
+    'TMPL_ACTION_SUCCESS'   =>  MODULE_PATH.'View/Public/success.html', // 默认成功跳转对应的模板文件
 );
