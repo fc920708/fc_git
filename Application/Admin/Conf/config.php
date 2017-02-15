ca774ba74810ee209701c37a045e76719cea30c3
fengchao <280315697@qq.com>
2017-02-15 10:03:50 +0800

    admin config

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,4 +1,20 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    //模板布局配置
+    'LAYOUT_ON'=>true,
+    'LAYOUT_NAME'=>'Layout/base',
+    
+    'SESSION_PREFIX'        =>  'Admin_',
+    
+    'TMPL_PARSE_STRING'  =>array(
+			'__JS__'    => '/Public/Home/js',
+			'__CSS__'    => '/Public/Home/css',
+			'__IMG__'    => '/Public/Home/image',
+			'__KINDEDITOR__' => '/Public/Kindeditor',
+	        '__BOOTSTRAP__' => '/Public/Bootstrap',
+	        '__CUBE__' => '/Public/Cube',
+			'__PUBLIC__' =>'/Public',
+    
+    ),
 );
