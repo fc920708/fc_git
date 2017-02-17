ee8cb8d339d4b73ee7ba239db62e17edfdbab2e4
fengchao <280315697@qq.com>
2017-02-17 17:46:52 +0800

    index

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,9 +1,82 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
+use Admin\Common\Constants\MessageConstants;
+use Admin\Common\Constants\NumberConstants;
+
+class IndexController extends Controller
+{
+
+    protected function _initialize()
+    {
         layout(false);
-        $this->display();
     }
+
+    //µÇÂ¼
+    public function index()
+    {
+        if(session('?uid')){
+//             redirect('/Admin/Base/index');
+        }
+        if(IS_GET) {
+//             dump(session());
+            $this->display();
+        }else{
+            $username = I('post.username');
+            $password = I('post.password');
+
+            $userModel = D('User');
+            //dump($userModel);
+            $userInfo = $userModel->getUserByName($username);
+//             dump($userModel->where("username='".$username."'")->find());die;
+            if(empty($userInfo)) {
+                $response['status'] = NumberConstants::ACTION_ERROR;
+                $response['content'] = MessageConstants::ADMIN_LOGIN_USER_NOT_EXISTS;
+                $this->ajaxReturn($response);
+            }elseif($userInfo['status'] != 1){
+                $response['status'] = NumberConstants::ACTION_ERROR;
+                $response['content'] = MessageConstants::ADMIN_LOGIN_USER_STATUS_ERROR;
+                $this->ajaxReturn($response);
+
+            }elseif(md5($userInfo['password']) != md5($password)) {
+                $response['status'] = NumberConstants::ACTION_ERROR;
+                $response['content'] = MessageConstants::ADMIN_LOGIN_PWD_ERROR;
+                $this->ajaxReturn($response);
+            }else{
+                $response['status'] = NumberConstants::ACTION_SUCCESS;
+                $response['content'] = MessageConstants::ADMIN_LOGIN_SUCCESS;
+                $response['url'] = '/Admin/Base/index';
+                   
+                //ÉèÖÃµÇÂ¼
+                session('uid',$userInfo['uid']);
+                session('username',$userInfo['username']);
+
+                $this->ajaxReturn($response);
+            }
+
+        }
+    }
+
+    public function logout()
+    {
+        session(null);
+        cookie(null);
+        redirect('/Admin');
+    }
+
+
+    public function test()
+    {
+//        session('aaaaa',1);
+//        //sleep(5);
+//        echo session('aaaaa');
+
+        dump($_SESSION);
+    }
+
+
+
+
+
+}
 }
