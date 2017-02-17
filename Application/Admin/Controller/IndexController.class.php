a796228cc40d05420a629bde5c96bc549e5f8c71
fengchao <280315697@qq.com>
2017-02-17 17:39:09 +0800

    index

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Admin\Controller;
-// use Think\Controller;
-class IndexController extends BaseController {
+use Think\Controller;
+class IndexController extends Controller {
     public function index(){
         layout(false);
         $this->display();
